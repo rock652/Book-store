ff514edea99c8c3bff0a2fbbe6436aba931c8ea0
Rahul Yadav <rahulyadav989290@gmail.com>
2024-09-18 18:44:45 +0530

    project completed frontend nad backend

---
+++ backend/index.js
@@ -0,0 +1,47 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
+import booksRoute from "./routes/booksRoutes.js";
+import path from "path";
+import { connectDB } from "./config/db.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5555;
+
+//middleware for parsing request body
+
+app.use(express.json());
+
+//middleware for handling cors policy
+// option 1 : -Allow all origins with the default of cors
+app.use(cors());
+
+const __dirname = path.resolve();
+
+// option:-2 Allow custom origins
+
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000",
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     allowedHeaders: ["content-type"],
+//   })
+// );
+
+app.use("/books", booksRoute);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
+console.log(process.env.MONGO_URI);
+app.listen(PORT, () => {
+  connectDB();
+  console.log("server is listenoing at http://localhost:" + PORT);
+});
