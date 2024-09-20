d21f158066947b5c59a5444e2d3dbe200d3ce217
Rahul Yadav <rahulyadav989290@gmail.com>
2024-09-20 09:52:59 +0530

    password will changed  to new password

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-import mongoose from "mongoose";
+
 import cors from "cors";
 import booksRoute from "./routes/booksRoutes.js";
 import path from "path";
